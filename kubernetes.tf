30b5a52f4f5512731e5157e184664021fd052235
Valentin Ouvrard <valentin.ouvrard@nautile.sarl>
2023-08-31 10:24:15 +1100

    clean

--- kubernetes.tf
+++ kubernetes.tf
@@ -7,10 +7,10 @@ variable "letsencrypt_contact" {
 provider "kubernetes" {
   #load_config_file = "false"
 
-  host     = google_container_cluster.primary.endpoint
 #username = var.gke_username
 #password = var.gke_password
 
+  host     = google_container_cluster.primary.endpoint
   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
   client_key             = google_container_cluster.primary.master_auth.0.client_key
   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
@@ -91,31 +91,32 @@ resource "helm_release" "cert-manager" {
 
 }
 
-resource "kubernetes_manifest" "clusterissuer_letsencrypt_prod" {
-  manifest = {
-    "apiVersion" = "cert-manager.io/v1"
-    "kind" = "ClusterIssuer"
-    "metadata" = {
-      "name" = "letsencrypt-prod"
-    }
-    "spec" = {
-      "acme" = {
-        "email" = var.letsencrypt_contact
-        "preferredChain" = ""
-        "privateKeySecretRef" = {
-          "name" = "letsencrypt-prod"
-        }
-        "server" = "https://acme-v02.api.letsencrypt.org/directory"
-        "solvers" = [
-          {
-            "http01" = {
-              "ingress" = {
-                "class" = "nginx"
-              }
-            }
-          },
-        ]
-      }
-    }
-  }
-}
+#resource "kubernetes_manifest" "clusterissuer_letsencrypt_prod" {
+#  depends_on = [helm_release.cert-manager]
+#  manifest = {
+#    "apiVersion" = "cert-manager.io/v1"
+#    "kind" = "ClusterIssuer"
+#    "metadata" = {
+#      "name" = "letsencrypt-prod"
+#    }
+#    "spec" = {
+#      "acme" = {
+#        "email" = var.letsencrypt_contact
+#        "preferredChain" = ""
+#        "privateKeySecretRef" = {
+#          "name" = "letsencrypt-prod"
+#        }
+#        "server" = "https://acme-v02.api.letsencrypt.org/directory"
+#        "solvers" = [
+#          {
+#            "http01" = {
+#              "ingress" = {
+#                "class" = "nginx"
+#              }
+#            }
+#          },
+#        ]
+#      }
+#    }
+#  }
+#}
