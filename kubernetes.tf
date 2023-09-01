f3985a3427ccdf26b1147759dfaaa281b6398ff1
Valentin Ouvrard <valentin.ouvrard@nautile.sarl>
2023-09-01 15:01:49 +1100

    MultiZone Kubernetes

--- kubernetes.tf
+++ kubernetes.tf
@@ -98,32 +98,32 @@ resource "helm_release" "cert-manager" {
 
 }
 
-#resource "kubernetes_manifest" "clusterissuer_letsencrypt_prod" {
-#  depends_on = [helm_release.cert-manager]
-#  manifest = {
-#    "apiVersion" = "cert-manager.io/v1"
-#    "kind" = "ClusterIssuer"
-#    "metadata" = {
-#      "name" = "letsencrypt-prod"
-#    }
-#    "spec" = {
-#      "acme" = {
-#        "email" = var.letsencrypt_contact
-#        "preferredChain" = ""
-#        "privateKeySecretRef" = {
-#          "name" = "letsencrypt-prod"
-#        }
-#        "server" = "https://acme-v02.api.letsencrypt.org/directory"
-#        "solvers" = [
-#          {
-#            "http01" = {
-#              "ingress" = {
-#                "class" = "nginx"
-#              }
-#            }
-#          },
-#        ]
-#      }
-#    }
-#  }
-#}
+resource "kubernetes_manifest" "clusterissuer_letsencrypt_prod" {
+  depends_on = [helm_release.cert-manager]
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind" = "ClusterIssuer"
+    "metadata" = {
+      "name" = "letsencrypt-prod"
+    }
+    "spec" = {
+      "acme" = {
+        "email" = var.letsencrypt_contact
+        "preferredChain" = ""
+        "privateKeySecretRef" = {
+          "name" = "letsencrypt-prod"
+        }
+        "server" = "https://acme-v02.api.letsencrypt.org/directory"
+        "solvers" = [
+          {
+            "http01" = {
+              "ingress" = {
+                "class" = "nginx"
+              }
+            }
+          },
+        ]
+      }
+    }
+  }
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -4,4 +4,5 @@ region               = "australia-southeast1"
 cluster_name         = "gke"
 gke_num_nodes        = 1
 gke_node_type        = "e2-highcpu-4"
+letsencrypt_contact  = "contact@ouvrard.it"
 service_account_path = "/home/valentin/Documents/OpsNC/ops-397413-0ab2f33593a4.json"
